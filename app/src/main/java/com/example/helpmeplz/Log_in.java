ce99710435ebf0bf60a2f9ed8d6455be76b99265
jjowoo <1201wogus@naver.com>
2023-05-19 09:03:02 +0900

    재현 5/19

--- app/src/main/java/com/example/helpmeplz/Log_in.java
+++ app/src/main/java/com/example/helpmeplz/Log_in.java
@@ -1,14 +1,89 @@
 package com.example.helpmeplz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Log_in extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    EditText editText_email,editText_Password;
+    Button login_btn, signup_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        signup_btn = findViewById(R.id.button_sign_in);
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Sign_in.class);
+                startActivity(intent);
+            }
+        });
+
+        login_btn= findViewById(R.id.button_login);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!editText_email.getText().toString().equals("") && !editText_Password.getText().toString().equals("")) {
+                    loginUser(editText_email.getText().toString(), editText_Password.getText().toString());
+                } else {
+                    Toast.makeText(Log_in.this, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(Log_in.this, Menu.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                }
+            }
+        };
+    }
+
+    public void loginUser(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 로그인 성공
+                            Toast.makeText(com.example.helpmeplz.Log_in.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+
+                            firebaseAuth.addAuthStateListener(firebaseAuthListener);
+                        } else {
+                            // 로그인 실패
+                            Toast.makeText(com.example.helpmeplz.Log_in.this, "아이디 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/example/helpmeplz/Sign_in.java
+++ app/src/main/java/com/example/helpmeplz/Sign_in.java
@@ -10,5 +10,7 @@ public class Sign_in extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+
     }
 }
