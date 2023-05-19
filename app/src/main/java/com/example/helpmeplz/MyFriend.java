41320a2c1a08467db28a7be5cf027ff2960eaea8
JongYoon Won <64014435+zibb03@users.noreply.github.com>
2023-05-19 14:19:58 +0900

    first

---
+++ app/src/main/java/com/example/helpmeplz/MyFriend.java
@@ -0,0 +1,13 @@
+package com.example.helpmeplz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyFriend extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_friend);
+    }
+}
