02b1ee2b59d39d2c59485f4863bf46d114489a13
jjowoo <1201wogus@naver.com>
2023-05-19 09:24:42 +0900

    재현 5/19

--- app/src/main/java/com/example/helpmeplz/Sign_in.java
+++ app/src/main/java/com/example/helpmeplz/Sign_in.java
@@ -1,16 +1,73 @@
 package com.example.helpmeplz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Sign_in extends AppCompatActivity {
+    Button signin;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-
+        signin = findViewById(R.id.button_sign_in);
+        mAuth = FirebaseAuth.getInstance();
+        signin.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(android.view.View view) {
+                signUp();
+            }
+        });
 
     }
+    private void signUp(){
+        String name=((EditText)findViewById(R.id.editText_name)).getText().toString();
+        String email=((EditText)findViewById(R.id.editText_email)).getText().toString();
+        String password=((EditText)findViewById(R.id.editText_password)).getText().toString();
+        String password_confirm=((EditText)findViewById(R.id.editText_password_confirm)).getText().toString();
+
+        if(email.length()>0 && password.length()>0 && password_confirm.length()>0){
+            if(password.equals(password_confirm)){
+                //firestore에 email,password 저장
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("TAG", "createUserWithEmail:success");
+                                    Toast.makeText(Sign_in.this, "회원가입에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(Sign_in.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+            else{
+                Toast.makeText(Sign_in.this, "비밀번호가 일치하지 않습니다." ,Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(Sign_in.this, "아아디와 비밀번호를 확인해주세요." ,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
