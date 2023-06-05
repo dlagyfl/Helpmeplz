a2881d2534182b2d927b342bb44820a473182827
amied <amieddoll@naver.com>
2023-06-05 17:49:32 +0900

    hi

--- app/src/main/java/com/example/helpmeplz/AddFriend.java
+++ app/src/main/java/com/example/helpmeplz/AddFriend.java
@@ -13,7 +13,6 @@ import android.widget.TextView;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,12 +20,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class AddFriend extends AppCompatActivity {
-    private String friendId;
+    private String friendUID;
     private String friendName;
-    private String userId;
-    private String userName;
-
     private DatabaseReference database;
+    private FirebaseAuth firebaseAuth;
     TextView view;
 
     @Override
@@ -34,42 +31,26 @@ public class AddFriend extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
 
+        database = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+
         view = findViewById(R.id.textView_friend_list);
         Button button_Accept = findViewById(R.id.button_accept);
         Button button_Refuse = findViewById(R.id.button_refuse);
 
-        database = FirebaseDatabase.getInstance().getReference();
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user != null){
-            userId = user.getUid();
-            Log.d("uid", userId);
-        }
-        database.child("users").child(userId).child("name").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Log.d("MainActivity", "ValueEventListener - onDataChange : " + (String) dataSnapshot.getValue());
-                userName = (String) dataSnapshot.getValue();
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e("Firebase", "Error fetching friends: " + databaseError.getMessage());
-            }
-        });
-
         button_Accept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("MainActivity", "onCreate - onClick : " + friendId);
+                Log.d("MainActivity", "onCreate - onClick : " + friendUID);
                 // 친구 DB에 내 UID 넣기
-                database.child("users").child(friendId).child("friendlist").child(userId).child("name").setValue(userName);
+                database.child("users").child(friendUID).child("friendlist").child("IYFpUCw26AQRYfKTVgoRazAR1oC2").child("name").setValue("test2");
                 // 내 DB에 친구 UID 넣기
-                database.child("users").child(userId).child("friendlist").child(friendId).child("name").setValue(friendName);
+                database.child("users").child("IYFpUCw26AQRYfKTVgoRazAR1oC2").child("friendlist").child(friendUID).child("name").setValue(friendName);
                 // 내 DB의 friendrequest에서 친구 추가한 UID 삭제하기
                 Log.d("MainActivity", "onCreate - onClick : " + 1);
-                database.getRef().child("users").child(userId).child("friendrequest").child(friendId).removeValue();
+                database.getRef().child("users").child("IYFpUCw26AQRYfKTVgoRazAR1oC2").child("friendrequest").child(friendUID).removeValue();
                 Log.d("MainActivity", "onCreate - onClick : " + 2);
-//                database.child("users").child(userId").child("friendrequest").child(friendUID).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+//                database.child("users").child("IYFpUCw26AQRYfKTVgoRazAR1oC2").child("friendrequest").child(friendUID).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
 //                    @Override
 //                    public void onSuccess(Void aVoid) {
 //                        Intent myIntent = new Intent(AddFriend.this, CheckFriend.class);
@@ -83,18 +64,17 @@ public class AddFriend extends AppCompatActivity {
 //                    }
 //                });
 
-                Log.d("MainActivity", "onCreate - onClick : " + friendName + " before intent");
-                Intent intent = new Intent(AddFriend.this, CheckFriend.class);
-                intent.putExtra("nickname", friendName);
-                startActivity(intent);
+                Intent myIntent3 = new Intent(AddFriend.this, CheckFriend.class);
+                Log.d("MainActivity", "onCreate - onClick : " + 3);
+                startActivity(myIntent3);
                 finish();
-                Log.d("MainActivity", "onCreate - onClick : " + friendName + " after intent");
             }
         });
         button_Refuse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                database.child("users").child(userId).child("friendrequest").child(friendId).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                database.child("users").child("IYFpUCw26AQRYfKTVgoRazAR1oC2").child("friendrequest").child(friendUID).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    // 38D30CEbX4ew1Hj3QcIuG0Ea6WH2
                     @Override
                     public void onSuccess(Void aVoid) {
 
@@ -108,44 +88,47 @@ public class AddFriend extends AppCompatActivity {
 
                 Intent myIntent2 = new Intent(AddFriend.this, MyFriend.class);
                 startActivity(myIntent2);
-//                finish();
+                finish();
             }
         });
 
+//        getGroups();
         getUID();
     }
 
     private void getUID() {
-
-        database.child("users").child(userId).child("friendrequest").addValueEventListener(new ValueEventListener() {
+        database.child("users").child("IYFpUCw26AQRYfKTVgoRazAR1oC2").child("friendrequest").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    friendId = snapshot.getKey();
-                    Log.d("MainActivity", "ValueEventListener - onDataChange : " + friendId);
+                    friendUID = snapshot.getKey();
+                    Log.d("MainActivity", "ValueEventListener - onDataChange : " + friendUID);
                 }
-                Log.d("MainActivity", "ValueEventListener - onDataChange : " + friendId);
-                getName(friendId);
+                Log.d("MainActivity", "ValueEventListener - onDataChange : " + friendUID);
+                getName(friendUID);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e("Firebase", "Error fetching friends: " + databaseError.getMessage());
+                Log.e("Firebase", "Error fetching groups: " + databaseError.getMessage());
             }
         });
     }
     private void getName(String UID) {
-        database.child("users").child(UID).child("name").addValueEventListener(new ValueEventListener() {
+        database.child("search").child(UID).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Log.d("MainActivity", "ValueEventListener - onDataChange : " + (String) dataSnapshot.getValue());
-                friendName = (String) dataSnapshot.getValue();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    friendName = (String) snapshot.getValue();
+                    Log.d("MainActivity", "ValueEventListener - onDataChange : " + friendName);
+                }
+                Log.d("MainActivity", "ValueEventListener - onDataChange : " + friendName);
                 view.setText(friendName);
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e("Firebase", "Error fetching friends: " + databaseError.getMessage());
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
