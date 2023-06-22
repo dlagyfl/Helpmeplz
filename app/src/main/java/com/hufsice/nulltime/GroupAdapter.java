748b5eae3845b43d6529080016f955033dcb2627
jukim4 <wkdwn0414@naver.com>
2023-06-22 15:10:05 +0900

    주영 주석

--- app/src/main/java/com/hufsice/nulltime/GroupAdapter.java
+++ app/src/main/java/com/hufsice/nulltime/GroupAdapter.java
@@ -12,9 +12,14 @@ import com.hufsice.nulltime.R;
 
 import java.util.List;
 
+
+//그룹 목록을 보여주기 위한 RecyclerView의 어댑터
+//GroupList activity에서 그룹 목록을 표시하고, 각 항목의 이벤트를 처리하기 위해 필요
 public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHolder> {
 
+    //그룹 목록 데이터를 담는 리스트
     private List<String> groupList;
+    //그룹 항목 클릭 이벤트 리스너
     private GroupItemClickListener itemClickListener;
 
     public GroupAdapter(List<String> groupList, GroupItemClickListener itemClickListener) {
@@ -25,27 +30,36 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
     @NonNull
     @Override
     public GroupViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //뷰 홀더 생성 시 호출되는 메소드
+        //레이아웃 파일을 inflate하여 뷰 객체를 생성하고, 이를 GroupViewHolder에 넘겨줌
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_group, parent, false);
         return new GroupViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull GroupViewHolder holder, int position) {
+        //뷰 홀더와 데이터를 바인딩하는 메소드
+        //해당 position에 있는 그룹명을 가져와서 뷰 홀더에 표시함
         String groupName = groupList.get(position);
         holder.bind(groupName);
     }
 
     @Override
     public int getItemCount() {
+        //데이터 개수 반환
         return groupList.size();
     }
 
     public class GroupViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+        //그룹 항목에 대한 뷰 홀더 클래스
 
+        //그룹명을 표시하는 텍스트뷰
         private TextView textViewGroupName;
 
         public GroupViewHolder(@NonNull View itemView) {
             super(itemView);
+            //뷰 홀더 생성 시 호출되는 메소드
+            //뷰 객체의 참조를 가져와서 변수에 저장하고, 클릭 이벤트 리스너를 등록함
             textViewGroupName = itemView.findViewById(R.id.textViewGroupName);
 
             itemView.setOnClickListener(this);
@@ -53,11 +67,15 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
         }
 
         public void bind(String groupName) {
+            //뷰 홀더에 데이터를 바인딩하는 메소드
+            //받아온 그룹명을 텍스트뷰에 설정함
             textViewGroupName.setText(groupName);
         }
 
         @Override
         public void onClick(View v) {
+            //그룹 항목을 클릭했을 때 호출되는 메소드
+            //해당 위치(position)을 이벤트 리스너에 전달함
             int position = getAdapterPosition();
             if (position != RecyclerView.NO_POSITION) {
                 itemClickListener.onGroupItemClick(position);
@@ -66,6 +84,8 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
 
         @Override
         public boolean onLongClick(View v) {
+            //그룹 항목을 롱클릭했을 때 호출되는 메소드
+            //해당 위치(position)을 이벤트 리스너에 전달하고, 롱클릭 이벤트를 소비함
             int position = getAdapterPosition();
             if (position != RecyclerView.NO_POSITION) {
                 itemClickListener.onGroupItemLongClick(position);
@@ -75,7 +95,11 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
     }
 
     public interface GroupItemClickListener {
+        //그룹 항목 클릭 이벤트 리스너 인터페이스
+
+        //그룹 항목을 클릭했을 때 호출되는 메소드
         void onGroupItemClick(int position);
+        //그룹 항목을 롱클릭했을 때 호출되는 메소드
         void onGroupItemLongClick(int position);
     }
 }
--- app/src/main/java/com/hufsice/nulltime/MyGroup.java
+++ app/src/main/java/com/hufsice/nulltime/MyGroup.java
@@ -30,64 +30,73 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyGroup extends AppCompatActivity {
+    //레이아웃에서 사용하는 뷰 요소들의 변수 선언
     private Button find_null_time;
     private ListView listView_member;
     private TextView txtView_groupName;
-    private ArrayAdapter<Object> groupListAdapter;
-
+    private ImageButton imbtn_exit;
+    //Firebase 관련 객체들의 변수 선언
     private FirebaseAuth firebaseAuth;
     private DatabaseReference reference;
+    private ArrayAdapter<Object> groupListAdapter;
     private String groupId;
-    private ImageButton imbtn_exit;
     List<String> memberList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_group);
 
+        //변수 연결
         find_null_time = findViewById(R.id.find_null_time);
         listView_member = findViewById(R.id.listView_member);
         txtView_groupName = findViewById(R.id.txtView_groupName);
         imbtn_exit = findViewById(R.id.imbtn_exit);
+        //Firebase 실시간 데이터베이스와 Firebase 인증을 사용하기 위해 필요한 초기화
         firebaseAuth = FirebaseAuth.getInstance();
         reference = FirebaseDatabase.getInstance().getReference();
 
+        // GroupList activity에서 보낸 그룹명과 그룹 id 받아오기
         String groupName = getIntent().getStringExtra("groupName");
-        groupId = getIntent().getStringExtra("groupId"); //groupId 가지고 오기
+        groupId = getIntent().getStringExtra("groupId");
 
+        //
         groupListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        //그룹 멤버 정보 가져오기
         getGroupMember(groupId);
+        //
         listView_member.setAdapter(groupListAdapter);
 
+        //groupList activity에서 선택한 그룹에 대한 이름 textView로 보여주기
         txtView_groupName.setText(groupName);
 
-        Intent intent1 = getIntent();
-        String groupID = intent1.getStringExtra("groupId");
+        //현재 로그인한 사용자 id userId에 저장
         String userId = firebaseAuth.getCurrentUser().getUid();
-        DatabaseReference membersRef = FirebaseDatabase.getInstance().getReference().child("groups").child("users").child(userId).child(groupID).child("members");
-
+        //그룹 멤버에 대해 접근, datasnapshot으로 멤버 정보 읽기
+        DatabaseReference membersRef = FirebaseDatabase.getInstance().getReference().child("groups").child("users").child(userId).child(groupId).child("members");
         membersRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-
-
+                //모든 멤버에 대해서 멤버 id memberList에 추가
+                //어디에 사용하나여..? 이거 뭐람 배고프다
                 for (DataSnapshot memberSnapshot : dataSnapshot.getChildren()) {
                     String memberId = memberSnapshot.getKey();
                     memberList.add(memberId);
                 }
-
+                //Null 시간 찾기 기능 실행
                 findNullTime();
 
             }
 
+            //데어터 읽기 실패시에 오류 발생
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 // 에러 처리
-                Log.e("Firebase", "Error fetching members: " + databaseError.getMessage());
+                Log.e("Firebase", databaseError.getMessage());
             }
         });
 
+        //그룹 삭제 여부 묻기 버튼
         imbtn_exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -107,29 +116,38 @@ public class MyGroup extends AppCompatActivity {
 //        });
     }
 
+    //그룹정보에 저장되어 있는 멤버 정보 읽기 메소드
     private void getGroupMember(String groupId) {
+        //현재 로그인되어 있는 사용자 id를 userId에 저장
         String userId = firebaseAuth.getCurrentUser().getUid();
+        //그룹 멤버 정보에 접근
         reference.child("groups").child("users").child(userId).child(groupId).child("members").addValueEventListener(new ValueEventListener() {
+            //datasnapshot으로 멤버 정보 읽기
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //멤버 정보 저장 list
                 List<Object> members = new ArrayList<>();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    //멤버 정보 member에 저장
                     Member member = snapshot.getValue(Member.class);
+                    //멤버 list에 반복으로 모든 멤버 이름 저장
                     members.add(member.getName());
                 }
+                //ArrayAdapter에 멤버 리스트 설정 및 갱신
                 groupListAdapter.clear();
                 groupListAdapter.addAll(members);
                 groupListAdapter.notifyDataSetChanged();
             }
 
-
+            //실패시에 오류발생
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e("Firebase", "Error fetching groups: " + databaseError.getMessage());
+                Log.e("Firebase", databaseError.getMessage());
             }
         });
     }
 
+    //공강시간 찾기 메소드
     private void findNullTime() {
         find_null_time.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -142,69 +160,78 @@ public class MyGroup extends AppCompatActivity {
         });
     }
 
+    //그룹 삭제 여부 묻기 메소드
     private void showLeaveGroupConfirmation() {
+        //현재 로그인한 user id 저장
         String userId = firebaseAuth.getCurrentUser().getUid();
-        String groupId = getIntent().getStringExtra("groupId");
-
+        //방장 정보 가지고 오기(방장만 그룹을 삭제할 수 있다.)
         DatabaseReference hostRef = reference.child("groups").child("users").child(userId).child(groupId).child("host");
         hostRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 String host = dataSnapshot.getValue(String.class);
-
-                if(host != null) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MyGroup.this);
-                    if (host.equals(userId)) {
-                        builder.setMessage("그룹을 삭제하시겠습니까?")
-                                .setNegativeButton("예", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        deleteGroup(groupId);
-                                        dialog.dismiss(); // 대화상자 닫기
-                                        Toast.makeText(getApplicationContext(), "그룹이 삭제되었습니다.", Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(MyGroup.this, GroupList.class);
-                                        startActivity(intent);
-                                        finish();
-                                    }
-                                })
-                                .setPositiveButton("아니요", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        dialog.dismiss();
-                                    }
-                                });
-                        builder.create().show();
-                    } else {
-                        builder.setMessage("그룹을 삭제하시겠습니까?")
-                                .setPositiveButton("예", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        Toast.makeText(getApplicationContext(), "방장만 사용할 수 있는 기능입니다.", Toast.LENGTH_SHORT).show();
-                                        dialog.dismiss();
-                                    }
-                                })
-                                .setNegativeButton("아니요", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        dialog.dismiss(); // 대화상자 닫기
-                                    }
-                                });
-                        builder.create().show();
-                    }
-                } else {
-                    //몰라~~
+                //대화 상자
+                AlertDialog.Builder builder = new AlertDialog.Builder(MyGroup.this);
+                //방장 정보와 현재 로그인 되어 있는 사용자 정보가 같은 경우에 그룹 삭제 기능 사용가능
+                if (host.equals(userId)) {
+                    builder.setMessage("그룹을 삭제하시겠습니까?")
+                            .setNegativeButton("예", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    //방정 정보 일치시에 그룹 삭제 메서드 발생
+                                    deleteGroup(groupId);
+                                    //대화상자 닫기
+                                    dialog.dismiss();
+                                    //그룹삭제 성공 toast발생
+                                    Toast.makeText(getApplicationContext(), "그룹이 삭제되었습니다.", Toast.LENGTH_SHORT).show();
+                                    //GroupList acitivity로 intent
+                                    Intent intent = new Intent(MyGroup.this, GroupList.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            })
+                            //'아니요' 선택시 대화상자가 닫히고 어떤 동작도 일어나지 않음
+                            .setPositiveButton("아니요", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    builder.create().show();
+                } //방장 정보와 현재 로그인 정보가 다른 경우 그룹 삭제 기능 사용 불가
+                else {
+                    builder.setMessage("그룹을 삭제하시겠습니까?")
+                            .setPositiveButton("예", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    //방장만 사용할 수 있다는 toast 발생
+                                    Toast.makeText(getApplicationContext(), "방장만 사용할 수 있는 기능입니다.", Toast.LENGTH_SHORT).show();
+                                    //대화상자 닫기
+                                    dialog.dismiss();
+                                }
+                            })
+                            //'아니요' 선택시 대화상자가 닫히고 어떤 동작도 일어나지 않음
+                            .setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    builder.create().show();
                 }
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 // 데이터 읽기가 실패한 경우 호출되는 메서드
-                Log.e("Firebase", "Error fetching host: " + databaseError.getMessage());
+                Log.e("Firebase",  databaseError.getMessage());
             }
         });
     }
 
+    //그룹 삭제 메소드
     private void deleteGroup(String groupId) {
         String userId = firebaseAuth.getCurrentUser().getUid();
         reference.child("groups").child("users").child(userId).child(groupId).child("members").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    //DB 탐색하면서 현재 그룹 id 정보 삭제
 
                     String memberId = snapshot.getKey();
                     DatabaseReference memberRef = FirebaseDatabase.getInstance().getReference();
@@ -223,9 +250,10 @@ public class MyGroup extends AppCompatActivity {
                             });
                 }
             }
+            //data 접근 실패시 오류 발생
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e("Firebase", "Error fetching groups: " + databaseError.getMessage());
+                Log.e("Firebase", databaseError.getMessage());
             }
         });
 
